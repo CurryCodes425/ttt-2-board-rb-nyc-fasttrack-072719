57afe85155aab07e414eb439fe34faf153f55e56
Casey (Bevin) Curry <c.larsoncurry425@gmail.com>
2019-07-28 17:43:08 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,7 @@
 # Define the variable board below.
+def  board = Array.new = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+  puts 'board'
+
+  puts " #{board[0]} | #{board[1]} | #{board[2]}"
+  puts "------------"
+end
